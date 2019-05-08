66ea7924f2654857d3817a1fc2a19216ec829a5b
LAPTOP-UMJUHS2V\code.developer07 <code.developer07@finocompany.com>
2019-05-08 17:05:12 -0400

    fixed input

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
+/*
+    The purpose of this project is reimplementing basic algorithm of converting and deconverting between Decimals and Binary base systems
+    instead of using native convertion functions such as toString(2); && toString(10) 
+*/
 
 'use strict';
 
 function openToBinaryModal() {
+    hideModalContents();
+    document.getElementById('modalInputToBin').style.display = 'unset';
     $('#toBinaryModal').modal('show');
-    document.getElementById('binaryModal').style.display = 'unset';
-    document.getElementById('toBinModalInput').value = 0;
 }
 
 function openToDenaryModal() {
+    hideModalContents();
+    document.getElementById('modalInputToDen').style.display = 'unset';
     $('#toBinaryModal').modal('show');
-    document.getElementById('toBinModalInput').value = 0;
 }
 
 function main() {
@@ -21,28 +26,36 @@ function main() {
     }
 }
 
-function validateInput() {
-    let denary = document.getElementById('toBinModalInput').value;
-    if(!denary) {
-        // swal({
-        //     title: "Wrong type!",
-        //     text: "it must be a number!",
-        //     icon: "warning",
-        //     button: "Understood",
-        //   }).then(function (value) {
-        //       $('#toBinModalInput').val(0);
-        //   });
-
-        //   $('#toBinModalInput').val(0);
-        return null;
-    } else {
-        denary = Number(denary);
-        return denary;
+function validateInput(isBin, modalValue) {
+    if(isBin) { 
+        if(!modalValue) {
+            return null;
+        } else {
+            return Number(modalValue);
+        }
+    } else if (!isBin) {
+        if(!modalValue) {
+            //modified because i do not use it anymore
+            // swal({
+            //     title: "Wrong type!",
+            //     text: "it must be a number!",
+            //     icon: "warning",
+            //     button: "Understood",
+            //   }).then(function (value) {
+            //       $('#modalInputToBin').val(0);
+            //   });
+    
+            //   $('#modalInputToBin').val(0);
+            return null;
+        } else {
+            return Number(modalValue);
+        }
     }
+    
 }
 
 function convertToBinary() {
-    let denary = validateInput();
+    let denary = validateInput(false, document.getElementById('modalInputToBin').value);
 
     if(denary) {
         let binary = [];
@@ -53,11 +66,24 @@ function convertToBinary() {
         }while(denary > 0);
         
         binary = binary.reverse().join('');
-        document.getElementById('toBinaryResult').innerText = binary;
+        document.getElementById('results').innerText = binary;
+    }    
+}
+
+function convertToDenary() {
+    let binary = validateInput(true, document.getElementById('modalInputToDen').value);
+    console.log('the binary input gives', binary, typeof(binary));
+    if(binary) {
+        binary = binary.toString();
+        document.getElementById('results').innerText = binary;
     }    
 }
 
 //hides all the inputs of the diff. modals
 function hideModalContents() {
-    document.getElementById('binaryModal').style.display = 'hidden';
+    document.getElementById('modalInputToBin').style.display = 'none';
+    document.getElementById('modalInputToDen').style.display = 'none';
+    document.getElementById('modalInputToBin').value = null;
+    document.getElementById('modalInputToDen').value = null;
+    document.getElementById('results').innerText = '';
 }
