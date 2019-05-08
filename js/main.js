8c14e38ec4fb8b9f8f623e1de6f029f953cbe98a
LAPTOP-UMJUHS2V\code.developer07 <code.developer07@finocompany.com>
2019-05-08 14:55:26 -0400

    +finished the convert binary functionality +removed sweetalert because ti affected the usability +modified the style a bit to make it more responsive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,53 @@
 
-function convertToBinary() {
+function openToBinaryModal() {
     $('#toBinaryModal').modal('show');
+    document.getElementById('toBinModalInput').value = 0;
 }
 
 function convertToDenary() {
 }
 
 function main() {
-    console.log('Page loaded')
+    console.log('Page loaded');
+    if($()) {
+        console.log('JQuery was loaded')
+    } else {
+        console.log('Error, JQuery was NOT loaded')
+    }
+}
+
+function validateInput() {
+    let denary = document.getElementById('toBinModalInput').value;
+    if(!denary) {
+        // swal({
+        //     title: "Wrong type!",
+        //     text: "it must be a number!",
+        //     icon: "warning",
+        //     button: "Understood",
+        //   }).then(function (value) {
+        //       $('#toBinModalInput').val(0);
+        //   });
+
+        //   $('#toBinModalInput').val(0);
+        return null;
+    } else {
+        denary = Number(denary);
+        return denary;
+    }
+}
+
+function convertToBinary() {
+    let denary = validateInput();
+
+    if(denary) {
+        let binary = [];
+
+        do {
+            binary.push(parseInt(denary % 2) );
+            denary = parseInt(denary / 2);
+        }while(denary > 0);
+        
+        binary = binary.reverse().join('');
+        document.getElementById('toBinaryResult').innerText = binary;
+    }    
 }
